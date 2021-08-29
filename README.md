735af274d39c9eb243df808fff2ff394457ddfd2
Ajit Kumar <ajitkumar3259@gmail.com>
2021-08-29 14:41:20 +0530

    Fixing canvas size and adding download image button

---
+++ README.md
@@ -0,0 +1,5 @@
+Circle Patterns
+==========
+
+Visualize Epicycloid patterns on a circle. 
+This provides interactive animation to users and lets them download a particular pattern.
--- drawing.js
+++ drawing.js
@@ -1,32 +1,26 @@
-var canvas = document.getElementById("board");
-var draw_heading = document.getElementById("draw");
-var width = window.innerWidth; //cuurent width of browser window
-var height = window.innerHeight; //current height of browser window
+let canvas = document.getElementById("responsive-canvas");
+let draw_heading = document.getElementById("draw");
 
-canvas.setAttribute("width", width - 100);
-canvas.setAttribute("height", height);
-var context = canvas.getContext("2d");
+const colors = ["#4a4e4d", "#0e9aa7", "#3da4ab", "#f6cd61", "#fe8a71"];
 
-const center_x = width/2 - 50;
-const center_y = height/2 - 50;
-const radius = 300;
+let context = canvas.getContext("2d");
 
-function drawCircle() {
+function drawCircle(center_x, center_y, radius) {
 	context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
 }
 
-function getPointOnCircle(angle) {
-	var x = center_x + radius * Math.cos(angle);
-	var y = center_y + radius * Math.sin(angle);
+function getPointOnCircle(center_x, center_y, radius, angle) {
+	let x = center_x + radius * Math.cos(angle);
+	let y = center_y + radius * Math.sin(angle);
 
 	return [x, y]
 }
 
-function getPointsOnCircle(noOfPoints) {
+function getPointsOnCircle(center_x, center_y, radius, noOfPoints) {
 	let points = {};
 	let angle = 0;
 	for(let i =0;i < noOfPoints; i++) {
-		let point = getPointOnCircle(angle + i * 2 * Math.PI / noOfPoints);
+		let point = getPointOnCircle(center_x, center_y, radius, angle + i * 2 * Math.PI / noOfPoints);
 		points[i] = point;
 	}
 	return points;
@@ -37,8 +31,8 @@ function drawLine(point1, point2) {
 	context.lineTo(point2[0], point2[1]);
 }
 
-function drawPattern(noOfPoints, times) {
-	let points = getPointsOnCircle(noOfPoints);
+function drawPattern(center_x, center_y, radius, noOfPoints, times) {
+	let points = getPointsOnCircle(center_x, center_y, radius, noOfPoints);
 
 	for(let i = 1; i < noOfPoints; i++) {
 		let curPoint = i;
@@ -56,11 +50,23 @@ function drawPattern(noOfPoints, times) {
 function draw(noOfPoints, times) {
 	if(times <= 1)
 		return;
+	canvas.width = window.innerWidth;
+	canvas.height = Math.max(window.innerWidth * 0.6, window.innerHeight * 0.6);
+	let width = canvas.width;
+	let height = canvas.height;
 	draw_heading.innerText = "Number of Points = " + noOfPoints; 
 	//console.log(new Date().getTime(), "animate called for points = ", noOfPoints);
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.lineWidth = 2;
+	//context.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
 	context.beginPath();
-	drawCircle();
-	drawPattern(noOfPoints, times);
+
+	let center_x = width/2;
+	let center_y = height/2;
+	let radius = Math.min(width/2.1, height/2.1);
+	drawCircle(center_x, center_y, radius);
+	
+	drawPattern(center_x, center_y, radius, noOfPoints, times);
+	
 	context.stroke();
 }
